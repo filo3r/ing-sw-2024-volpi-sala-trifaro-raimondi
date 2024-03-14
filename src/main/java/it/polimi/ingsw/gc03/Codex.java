bf400c3f64cf3a6225576a5e112cfd198c96a2ca
M S <me@mtsl.me>
2024-03-14 15:49:10 +0100

    New fundamental classes

--- src/main/java/it/polimi/ingsw/gc03/Codex.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.gc03;
-
-public class Codex {
-    /**
-     * Data structure of the codex.
-     */
-    private Card[][] codex;
-}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module it.polimi.ingsw.gc03 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens it.polimi.ingsw.gc03 to javafx.fxml;
-    exports it.polimi.ingsw.gc03;
-}
