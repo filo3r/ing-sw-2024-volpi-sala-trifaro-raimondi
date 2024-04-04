ab1207074b9505c5b671bb162d5e2575d029e180
matteosalaa <95040779+matteosalaa@users.noreply.github.com>
2024-04-04 12:33:27 +0200

    Fixed paths

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,18 +6,18 @@ module it.polimi.ingsw.gc03 {
 
     opens it.polimi.ingsw.gc03 to javafx.fxml;
     exports it.polimi.ingsw.gc03;
-    exports it.polimi.ingsw.gc03.Exceptions;
+    exports it.polimi.ingsw.gc03.model.Exceptions;
     opens it.polimi.ingsw.gc03.Exceptions to javafx.fxml;
-    exports it.polimi.ingsw.gc03.Card;
+    exports it.polimi.ingsw.gc03.model.Card;
     opens it.polimi.ingsw.gc03.Card to javafx.fxml, com.google.gson;
-    exports it.polimi.ingsw.gc03.Side;
+    exports it.polimi.ingsw.gc03.model.Side;
     opens it.polimi.ingsw.gc03.Side to javafx.fxml, com.google.gson;
-    exports it.polimi.ingsw.gc03.Side.Front;
+    exports it.polimi.ingsw.gc03.model.Side.Front;
     opens it.polimi.ingsw.gc03.Side.Front to javafx.fxml, com.google.gson;
-    exports it.polimi.ingsw.gc03.Side.Back;
+    exports it.polimi.ingsw.gc03.model.Side.Back;
     opens it.polimi.ingsw.gc03.Side.Back to javafx.fxml, com.google.gson;
-    exports it.polimi.ingsw.gc03.Enumerations;
+    exports it.polimi.ingsw.gc03.model.Enumerations;
     opens it.polimi.ingsw.gc03.Enumerations to javafx.fxml, com.google.gson;
-    exports it.polimi.ingsw.gc03.Card.CardObjective;
+    exports it.polimi.ingsw.gc03.model.Card.CardObjective;
     opens it.polimi.ingsw.gc03.Card.CardObjective to javafx.fxml, com.google.gson;
 }
