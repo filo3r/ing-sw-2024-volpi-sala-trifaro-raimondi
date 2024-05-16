699a983993ae7500838c41f6c3dbb890fe74f5e7
matteosalaa <95040779+matteosalaa@users.noreply.github.com>
2024-05-16 16:13:29 +0200

    Added codex to view implementation: still have to fix spaces between cards

--- src/main/java/it/polimi/ingsw/gc03/view/tui/Tui.java
+++ src/main/java/it/polimi/ingsw/gc03/view/tui/Tui.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.gc03.view.tui;
 
 import it.polimi.ingsw.gc03.controller.GameController;
+import it.polimi.ingsw.gc03.model.Codex;
 import it.polimi.ingsw.gc03.model.Game;
 import it.polimi.ingsw.gc03.model.enumerations.GameStatus;
 import it.polimi.ingsw.gc03.model.enumerations.Kingdom;
@@ -8,9 +9,11 @@ import it.polimi.ingsw.gc03.model.enumerations.Value;
 import it.polimi.ingsw.gc03.model.side.Side;
 import it.polimi.ingsw.gc03.model.side.back.BackGold;
 import it.polimi.ingsw.gc03.model.side.back.BackSide;
+import it.polimi.ingsw.gc03.model.side.back.BackStarter;
 import it.polimi.ingsw.gc03.model.side.front.FrontGold;
 import it.polimi.ingsw.gc03.model.side.front.FrontResource;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -22,17 +25,18 @@ public class Tui {
     private int screenWidth;
     private int screenHeight;
 
+    private int screenSimY = 364;
+    private int screenSimX = 1093;
+
+    private CharSpecial[][] middleScreen;
     private String[][] screenToPrint;
-
-    public void showCodex(Game game){
-        StringBuilder codexText = new StringBuilder();
-
-    }
 
     public void Tui(int width, int height){
         screenWidth = width;
         screenHeight = height;
-        screenSim = new CharSpecial[screenHeight][screenWidth];
+        // screenSim represents the whole codex: 81x81 -> 81*9 x 81*27 = 728x2187 since every side is 9x27
+        screenSim = new CharSpecial[729][2187];
+        middleScreen = new CharSpecial[screenHeight][screenWidth];
         screenToPrint = new String[screenHeight][screenWidth*3];
         for (int i = 0; i < screenHeight; i++) {
             for (int j = 0; j < screenWidth * 3; j++) {
@@ -40,21 +44,25 @@ public class Tui {
             }
         }
         clearScreen(' ');
-        // The following is just an initial test: it shows 5 cards, a possible starter card and 4 null cards
-        showSide(new FrontResource(Kingdom.FUNGI, Value.INSECT, Value.ANIMAL, Value.QUILL, Value.COVERED, 0), 23,74);
-        showSide(new BackGold(Kingdom.PLANT, Value.FUNGI, Value.COVERED, Value.INKWELL,Value.ANIMAL, new ArrayList<>(Arrays.asList(Value.ANIMAL))), 23,119);
-        showSide(new FrontGold(Kingdom.FUNGI, Value.COVERED, Value.EMPTY, Value.EMPTY,Value.EMPTY, 2, Value.INKWELL, new ArrayList<>(Arrays.asList(Value.ANIMAL, Value.FUNGI))), 35,119);
-        showSide(new BackSide(Kingdom.ANIMAL, Value.EMPTY, Value.QUILL, Value.COVERED,Value.EMPTY, new ArrayList<>(Arrays.asList(Value.ANIMAL))), 35,74);
-        showSide(new FrontResource(Kingdom.PLANT, Value.ANIMAL, Value.FUNGI, Value.ANIMAL,Value.FUNGI, 1), 29,97); // STARTER
-        while(true){
+        Codex codex = new Codex();
+        try {
+            codex.insertStarterIntoCodex(new BackStarter(Kingdom.NULL, Value.EMPTY, Value.EMPTY, Value.EMPTY, Value.EMPTY, new ArrayList<>(Arrays.asList(Value.ANIMAL, Value.INSECT))));
+            codex.insertIntoCodex(new FrontResource(Kingdom.PLANT, Value.ANIMAL, Value.FUNGI, Value.ANIMAL,Value.FUNGI, 1), 40+1,40+1);
+            codex.insertIntoCodex(new FrontResource(Kingdom.PLANT, Value.ANIMAL, Value.FUNGI, Value.ANIMAL,Value.FUNGI, 1), 40+2,40);
+            codex.insertIntoCodex(new FrontResource(Kingdom.PLANT, Value.ANIMAL, Value.FUNGI, Value.ANIMAL,Value.FUNGI, 1), 40+2,40+2);
+            codex.insertIntoCodex(new FrontResource(Kingdom.PLANT, Value.ANIMAL, Value.FUNGI, Value.ANIMAL,Value.FUNGI, 1), 40+3,40+3);
+            codex.insertIntoCodex(new FrontResource(Kingdom.PLANT, Value.ANIMAL, Value.FUNGI, Value.ANIMAL,Value.FUNGI, 1), 40+3,40+1);
+        } catch (RemoteException e) {
+            throw new RuntimeException(e);
         }
+        translateCodexToScreenSim(codex);
     }
 
     public void refreshScreen() {
         asyncClean();
-        getScreenToPrint();
+        getScreenToPrint(screenSimX, screenSimY);
         StringBuilder screenText = new StringBuilder();
-        for (int i = 0; i < screenHeight; i++) {
+        for (int i = 0; i < screenHeight-1; i++) {
             for (int j = 0; j < screenWidth; j++) {
                 screenText.append(screenToPrint[i][j * 3]).append(screenToPrint[i][j * 3 + 1]).append(screenToPrint[i][j * 3 + 2]);
             }
@@ -64,7 +72,11 @@ public class Tui {
     }
 
     public void clearScreen(char fillChar) {
-        char topLeft = '┌';
+        for(int i = 0; i < 729; i++) {
+            for(int j = 0; j < 2187; j++) {
+                screenSim[i][j] = new CharSpecial(CharColor.WHITE, fillChar);
+            }
+        }char topLeft = '┌';
         char topRight = '┐';
         char bottomLeft = '└';
         char bottomRight = '┘';
@@ -75,28 +87,28 @@ public class Tui {
             for (int j = 0; j < screenWidth; j++) {
                 if (i == 0) {
                     if (j == 0) {
-                        screenSim[i][j] = new CharSpecial(CharColor.WHITE, topLeft);
+                        middleScreen[i][j] = new CharSpecial(CharColor.WHITE, topLeft);
                     } else if (j == screenWidth - 1) {
-                        screenSim[i][j] = new CharSpecial(CharColor.WHITE, topRight);
+                        middleScreen[i][j] = new CharSpecial(CharColor.WHITE, topRight);
                     } else {
-                        screenSim[i][j] = new CharSpecial(CharColor.WHITE, horizontalLine);
+                        middleScreen[i][j] = new CharSpecial(CharColor.WHITE, horizontalLine);
                     }
                 } else if (i == screenHeight - 1) {
                     if (j == 0) {
-                        screenSim[i][j] = new CharSpecial(CharColor.WHITE, bottomLeft);
+                        middleScreen[i][j] = new CharSpecial(CharColor.WHITE, bottomLeft);
                     } else if (j == screenWidth - 1) {
-                        screenSim[i][j] = new CharSpecial(CharColor.WHITE, bottomRight);
+                        middleScreen[i][j] = new CharSpecial(CharColor.WHITE, bottomRight);
                     } else {
-                        screenSim[i][j] = new CharSpecial(CharColor.WHITE, horizontalLine);
+                        middleScreen[i][j] = new CharSpecial(CharColor.WHITE, horizontalLine);
                     }
                 } else if (j == 0 || j == screenWidth - 1) {
-                    screenSim[i][j] = new CharSpecial(CharColor.WHITE, verticalLine);
+                    middleScreen[i][j] = new CharSpecial(CharColor.WHITE, verticalLine);
                 } else {
-                    screenSim[i][j] = new CharSpecial(CharColor.WHITE, fillChar);
+                    middleScreen[i][j] = new CharSpecial(CharColor.WHITE, fillChar);
                 }
             }
         }
-        screenSim[screenHeight - 2][1] = new CharSpecial(CharColor.WHITE, '>');
+        middleScreen[screenHeight - 2][1] = new CharSpecial(CharColor.WHITE, '>');
         refreshScreen();
     }
 
@@ -107,7 +119,7 @@ public class Tui {
             for (int j = 0; j < 27; j++) {
                 int rowIndex = row + i;
                 int colIndex = col + j;
-                if (rowIndex > 0 && rowIndex < screenHeight && colIndex > 0 && colIndex < screenWidth) {
+                if (rowIndex > 0 && rowIndex < 729 && colIndex > 0 && colIndex < 2187) {
                     screenSim[rowIndex][colIndex] = sideArray[i][j];
                 }
             }
@@ -115,48 +127,77 @@ public class Tui {
         refreshScreen();
     }
 
-    private void getScreenToPrint() {
+    private void translateCodexToScreenSim(Codex codex){
+        // First check if starter card is positioned
+        if(!codex.getCardStarterInserted()){
+            return;
+        }
+
+        recursiveShowSide(codex.getCodex(), 40, 40);
+    }
+
+    private static boolean[][] visited = new boolean[81][81];
+    private  void recursiveShowSide(Side[][] codex, int i, int j) {
+        if (i < 0 || i >= 81 || j < 0 || j >= 81 || visited[i][j] || codex[i][j] == null) {
+            return;
+        }
+        visited[i][j] = true;
+        showSide(codex[i][j], i*9, j*27);
+        recursiveShowSide(codex, i-1, j-1);
+        recursiveShowSide(codex, i-1, j+1);
+        recursiveShowSide(codex, i+1, j-1);
+        recursiveShowSide(codex, i+1, j+1);
+    }
+
+
+
+    // (x,y) is the top left point of screenSim that will be showed to the user.
+    private void getScreenToPrint(int x , int y) {
+        if (y + screenHeight > 729) {
+            y = 729 - screenHeight;
+        }
+        if (x + screenWidth > 2187) {
+            x = 2187 - screenWidth;
+        }
+        if(x<0){
+            x=0;
+        }
+        if(y<0){
+            y=0;
+        }
+
+        // replace the middleScreen with the right portion on screenSim
+        for (int i = 1; i < screenHeight; i++) {
+            for (int j = 1; j < screenWidth; j++) {
+                middleScreen[i][j] = screenSim[i+y-screenHeight/2][j+x-screenWidth/2];;
+            }
+        }
+
         for (int i = 0; i < screenHeight; i++) {
             for (int j = 0; j < screenWidth; j++) {
-                CharSpecial charSpecial = screenSim[i][j];
+                CharSpecial charSpecial = middleScreen[i][j];
                 char c = charSpecial.c;
-                CharColor color = charSpecial.color;
-
-                String ansiCode;
-                switch (color) {
-                    case GREEN:
-                        ansiCode = "\u001B[32m";
-                        break;
-                    case RED:
-                        ansiCode = "\u001B[31m";
-                        break;
-                    case BLUE:
-                        ansiCode = "\u001B[34m";
-                        break;
-                    case MAGENTA:
-                        ansiCode = "\u001B[35m";
-                        break;
-                    case GOLD:
-                        ansiCode = "\u001B[33m";
-                        break;
-                    case WHITE:
-                        ansiCode = "\u001B[37m";
-                        break;
-                    case BLACK:
-                        ansiCode = "\u001B[30m";
-                        break;
-                    default:
-                        ansiCode = "\u001B[0m";
-                        break;
-                }
-
-
+                String ansiCode = getAnsiCode(charSpecial.color);
                 int index = j * 3;
                 screenToPrint[i][index] = ansiCode;
                 screenToPrint[i][index + 1] = String.valueOf(c);
                 screenToPrint[i][index + 2] = "\u001B[0m";
             }
         }
+    }
+
+    private static String getAnsiCode(CharColor color){
+        String ansiCode = switch (color) {
+            case GREEN -> "\u001B[32m";
+            case RED -> "\u001B[31m";
+            case BLUE -> "\u001B[34m";
+            case MAGENTA -> "\u001B[35m";
+            case GOLD -> "\u001B[33m";
+            case WHITE -> "\u001B[37m";
+            case BLACK -> "\u001B[30m";
+            default -> "\u001B[0m";
+        };
+        return ansiCode;
     }
 
     public void showTitle() {
